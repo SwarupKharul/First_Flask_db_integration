d1d127371eb60f7d8de28d94d0cef13159d39c02
Swarup Kharul <swarupkharulsk@gmail.com>
2021-04-30 14:07:50 +0530

    Updated: Modified models and add a new url at 'table/'

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-from flask import Flask, render_template, flash, request, redirect
-from flask_mysqldb import MySQL
-from flask_migrate import Migrate, MigrateCommand
-from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-from werkzeug.utils import secure_filename
-import os
 import glob
+import os
 import urllib.request
 
+import pandas as pd
+from flask import Flask, flash, redirect, render_template, request
+from flask_migrate import Migrate, MigrateCommand
+from flask_mysqldb import MySQL
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
+
 app = Flask(__name__)
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 UPLOAD_FOLDER = './static/uploads'
 
 app.secret_key = "Cairocoders-Ednalan"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/tryfirstdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
@@ -28,13 +28,14 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 
-# our model
 class UploadInfo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=False)
+    User_Account_ID = db.Column(db.Integer, primary_key=True)
+    Invoice_Number = db.Column(db.String(10))
+    Amount = db.Column(db.Integer)
+    Fees = db.Column(db.Integer)
 
-    def __init__(self, name):
-        self.name = name
+    def __repr__(self):
+        return f"User Account ID ('{self.User_Account_ID}')"
 
 
 ALLOWED_EXTENSIONS = set(['csv', 'xlsx'])
@@ -51,23 +52,43 @@ def upload_form():
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    # * means all if need specific format then *.csv
-    if request.method == 'POST':
-        filename = "da_che.xlsx"
-        files = request.files.getlist('files[]')
-        for file in files:
-            if file:
-                if allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    file.save(os.path.join(
-                        app.config['UPLOAD_FOLDER'], filename))
-                    info = UploadInfo(name=filename)
-                    db.session.add(info)
+    filename = "da_che.xlsx"
+    files = request.files.getlist('files[]')
+    for file in files:
+        if file:
+            if allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(
+                    app.config['UPLOAD_FOLDER'], filename))
+                df = pd.read_csv("./static/uploads/"+str(filename))
+                for i in range(df.shape[0]):
+                    if UploadInfo.query.get(df['User Account Id'][i]):
+                        info = UploadInfo.query.get(df['User Account Id'][i])
+                        info.User_Account_ID = df['User Account Id'][i]
+                        info.Invoice_Number = df['Invoice Number'][i]
+                        info.Amount = df['Amount'][i]
+                        info.Fees = df['Fees'][i]
+                        print("update")
+                    else:
+                        info = UploadInfo(
+                            User_Account_ID=df['User Account Id'][i],
+                            Invoice_Number=df['Invoice Number'][i],
+                            Amount=df['Amount'][i],
+                            Fees=df['Fees'][i])
+                        db.session.add(info)
+                        print("add")
                     db.session.commit()
-                else:
-                    flash('File extension not supported!')
-    content = {'file_name': filename}
-    return render_template('upload.html', **content)
+                print("Uploaded data successfully")
+            else:
+                flash('File extension not supported!')
+    return redirect('/')
+
+
+@app.route('/table/')
+def table():
+    data = UploadInfo.query.all()
+    content = {'data': data}
+    return render_template('table.html', **content)
 
 
 if __name__ == '__main__':
